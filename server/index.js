1a4992288d2d73d6bd3e0895faa0c48b5f2ea093
SaloniSS <sxs170015@utdallas.edu>
2020-06-06 10:01:25 -0500

    Node server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+var cors = require('cors')
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+    res.status(200).send('Hello, world!').end();
+  });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
