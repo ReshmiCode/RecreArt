6598a51a516a1f5b8f12652ad857f641843c6398
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-06-07 00:48:31 -0500

    styling for prof

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Header,
@@ -6,6 +6,7 @@ import {
   Body,
   Title,
   Text,
+  Thumbnail,
   Left,
   Right,
   Icon,
@@ -16,27 +17,25 @@ import PortfolioCard from "../components/PortfolioCard";
 import * as Google from "expo-google-app-auth";
 import { Image } from "react-native";
 
-import { ANDROID_CLIENT_ID, IOS_CLIENT_ID , ANDROID_APK_CLIENT_ID , IOS_APP_CLIENT_ID} from "../config";
+import {
+  ANDROID_CLIENT_ID,
+  IOS_CLIENT_ID,
+  ANDROID_APK_CLIENT_ID,
+  IOS_APP_CLIENT_ID,
+} from "../config";
 
 GLOBAL = require("../global");
 const axios = require("axios").default;
 
-var styles = {
-  button: {
-    width: 370,
-    margin: 10,
-    justifyContent: "center",
-  },
-};
-
 export default function ProfileScreen(props) {
-
   let [userInfo, setUserInfo] = useState({});
   let [userPhotos, setUserPhotos] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios(`https://hack-the-ne.appspot.com/api/v1/users/${GLOBAL.googleID}`);
+      const result = await axios(
+        `https://hack-the-ne.appspot.com/api/v1/users/${GLOBAL.googleID}`
+      );
       setUserInfo(result.data.data[0]);
       setUserPhotos(result.data.data[0].photos);
       //console.log("UserInfo");
@@ -46,34 +45,34 @@ export default function ProfileScreen(props) {
   }, []);
 
   const signOutWithGoogle = async () => {
-      try {
-        const { type, accessToken, user } = await Google.logOutAsync({
-          accessToken: GLOBAL.accessToken,
-          iosClientId: IOS_CLIENT_ID,
-          androidClientId: ANDROID_CLIENT_ID,
-          androidStandaloneAppClientId: ANDROID_APK_CLIENT_ID,
-          iosStandaloneAppClientId: IOS_APP_CLIENT_ID,
-          scopes: ["profile", "email"],
-        });
+    try {
+      const { type, accessToken, user } = await Google.logOutAsync({
+        accessToken: GLOBAL.accessToken,
+        iosClientId: IOS_CLIENT_ID,
+        androidClientId: ANDROID_CLIENT_ID,
+        androidStandaloneAppClientId: ANDROID_APK_CLIENT_ID,
+        iosStandaloneAppClientId: IOS_APP_CLIENT_ID,
+        scopes: ["profile", "email"],
+      });
 
-        console.log(type);
-        if (type === "success" || type === "default") {
-          GLOBAL.googleID = "";
-          GLOBAL.username = "";
-          GLOBAL.profilePic = "";
-          GLOBAL.accessToken = "";
-          GLOBAL.databaseID = "";
+      console.log(type);
+      if (type === "success" || type === "default") {
+        GLOBAL.googleID = "";
+        GLOBAL.username = "";
+        GLOBAL.profilePic = "";
+        GLOBAL.accessToken = "";
+        GLOBAL.databaseID = "";
 
-          console.log("Signed Out");
-          props.navigation.navigate("LogIn");
-          return accessToken;
-        } else {
-          return { cancelled: true };
-        }
-      } catch (e) {
-        console.log("Error with SignOut", e);
-        return { error: true };
+        console.log("Signed Out");
+        props.navigation.navigate("LogIn");
+        return accessToken;
+      } else {
+        return { cancelled: true };
       }
+    } catch (e) {
+      console.log("Error with SignOut", e);
+      return { error: true };
+    }
   };
 
   return (
@@ -97,27 +96,27 @@ export default function ProfileScreen(props) {
               <Text style={{ fontSize: 25, fontWeight: "bold" }}>Profile</Text>
             </Title>
           </Body>
-          <Body>
-            <Image
+          <Body style={{ flex: 1, flexDirection: "row", marginBottom: 20 }}>
+            <Thumbnail
               source={{
                 uri: userInfo.profilePic,
               }}
-              style={{ height: 300, width: 300, flex: 1 }}
+              style={{ height: 120, width: 100, flex: 0.5 }}
             />
+            <Body>
+              <Title>
+                <Text style={{ fontSize: 20 }}>{userInfo.userName}</Text>
+              </Title>
+              <Title>
+                <Text style={{ fontSize: 20 }}>
+                  {userPhotos.length} creations
+                </Text>
+              </Title>
+              <Button onPress={signOutWithGoogle}>
+                <Text> Logout </Text>
+              </Button>
+            </Body>
           </Body>
-          <Body>
-            <Title>
-            <Text style={{ fontSize: 25, fontWeight: "bold" }}>{userInfo.userName}</Text>
-            </Title>
-          </Body>
-          <Body>
-          <Title>
-            <Text style={{ fontSize: 25, fontWeight: "bold" }}>{userPhotos.length} creations</Text>
-            </Title>
-          </Body>
-          <Button style={styles.button} onPress={signOutWithGoogle}>
-            <Title> Logout </Title>
-          </Button>
           <Body>
             <Title>
               <Text style={{ fontSize: 25, fontWeight: "bold" }}>
@@ -125,10 +124,13 @@ export default function ProfileScreen(props) {
               </Text>
             </Title>
           </Body>
-          { userPhotos.map(function (photo, i) {
-              return (
-                  <PortfolioCard photo={ photo } key={ i }/>
-              );
+          {userPhotos.length == 0 && (
+            <Body>
+              <Text>No photos added yet.</Text>
+            </Body>
+          )}
+          {userPhotos.map(function (photo, i) {
+            return <PortfolioCard photo={photo} key={i} />;
           })}
         </Content>
       </LinearGradient>
