dd89f631a8eece926748ca7bdda6af8301a83e40
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-06-07 02:09:59 -0500

    vote and unvote

--- app/components/PhotoCard.js
+++ app/components/PhotoCard.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import { Card, CardItem ,Text , Body, Button} from "native-base";
+import React, { useState, useEffect } from "react";
+import { Card, CardItem, Text, Body, Button } from "native-base";
 import { Image } from "react-native";
 
 GLOBAL = require("../global");
@@ -8,10 +8,13 @@ const axios = require("axios").default;
 export default function PhotoCard(props) {
   let [photoInfo, setPhotoInfo] = useState({});
   let [photoVotes, setPhotoVotes] = useState(0);
+  let [voted, setVoted] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios(`https://hack-the-ne.appspot.com/api/v1/photos/${props.photo}`);
+      const result = await axios(
+        `https://hack-the-ne.appspot.com/api/v1/photos/${props.photo}`
+      );
       setPhotoInfo(result.data.data);
       setPhotoVotes(result.data.data.votes);
     }
@@ -19,41 +22,51 @@ export default function PhotoCard(props) {
   }, []);
 
   async function vote() {
-    setPhotoVotes(photoVotes+1);
-    const currentVote = photoVotes;
-    await axios.patch(`https://hack-the-ne.appspot.com/api/v1/photos/${props.photo}`,
-      {
-        votes: currentVote+1,
-      }
-    );
+    const currentVote = photoVotes + 1;
+    setPhotoVotes(currentVote);
+    await axios
+      .patch(`https://hack-the-ne.appspot.com/api/v1/photos/${props.photo}`, {
+        votes: currentVote,
+      })
+      .then(() => setVoted(true));
+  }
+
+  async function unvote() {
+    const currentVote = photoVotes - 1;
+    setPhotoVotes(currentVote);
+    await axios
+      .patch(`https://hack-the-ne.appspot.com/api/v1/photos/${props.photo}`, {
+        votes: currentVote,
+      })
+      .then(() => setVoted(false));
   }
 
   return (
     <Card>
-        <CardItem cardBody>
-          <Image
-            source={{
-              uri: photoInfo.userPhoto,
-            }}
-            style={{ height: 300, width: null, flex: 1 }}
-          />
-        </CardItem>
-        <CardItem>
-            <Body>
-              <Text style={{color: "#fff"}}>
-                Accuracy: {photoInfo.accuracy}
-              </Text>
-              <Text style={{color: "#fff"}}>
-                Mode: {photoInfo.mode}
-              </Text>
-              <Text style={{color: "#fff"}}>
-                Votes: {photoVotes}
-              </Text>
-            </Body>
-            <Button onPress={vote}>
-              <Text> Vote </Text>
-            </Button>
-        </CardItem>
+      <CardItem cardBody>
+        <Image
+          source={{
+            uri: photoInfo.userPhoto,
+          }}
+          style={{ height: 300, width: null, flex: 1 }}
+        />
+      </CardItem>
+      <CardItem>
+        <Body>
+          <Text style={{ color: "#fff" }}>Accuracy: {photoInfo.accuracy}</Text>
+          <Text style={{ color: "#fff" }}>Mode: {photoInfo.mode}</Text>
+          <Text style={{ color: "#fff" }}>Votes: {photoVotes}</Text>
+        </Body>
+        {voted ? (
+          <Button onPress={unvote}>
+            <Text> Unvote </Text>
+          </Button>
+        ) : (
+          <Button onPress={vote}>
+            <Text> Vote </Text>
+          </Button>
+        )}
+      </CardItem>
     </Card>
   );
 }
