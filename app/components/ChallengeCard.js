40305c3458553274069b3fb26c6a58c5e552a6cf
SaloniSS <sxs170015@utdallas.edu>
2020-06-06 22:31:34 -0500

    Challenge cards

--- app/components/ChallengeCard.js
+++ app/components/ChallengeCard.js
@@ -3,12 +3,13 @@ import { Card, CardItem } from "native-base";
 import { Image } from "react-native";
 
 export default function ChallengeCard(props) {
+  console.log(props.photo);
   return (
     <Card>
       <CardItem cardBody>
         <Image
           source={{
-            uri: "https://reactnative.dev/img/tiny_logo.png",
+            uri: props.photo.originalArt,
           }}
           style={{ height: 300, width: null, flex: 1 }}
         />
